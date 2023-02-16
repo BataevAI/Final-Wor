025c618e086f1fca3dbb3851bb736a80cdbb9243
Andrey Bataev <bataevai@gmail.com>
2023-02-16 22:46:51 +0400

    final

--- Exc. Total/Program.cs
+++ Exc. Total/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Вводим размер массива
+
+Console.WriteLine("Введите длину массива строк");
+
+int arraySize = Convert.ToInt32(Console.ReadLine());
+String[] Arr = new String[arraySize];
+
+//Вводим строчные элементы массива 
+
+Console.WriteLine($"Введите строки в количестве {arraySize}-ти");
+for(int i = 0; i < arraySize; i++) {
+
+Arr[i] = Console.ReadLine();
+
+}
